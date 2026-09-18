a6e7daba79dc91f62b410ae33bcbd93588ebf040
anushaa09 <sarkaranusha0909@gmail.com>
2026-09-18 22:48:03 +0530

    Connect dashboard to configurable API

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -1,9 +1,12 @@
+import os
 import streamlit as st
 import pandas as pd
 import requests
 
-
-API_URL = "http://localhost:8000"
+API_URL = os.getenv(
+    "API_URL",
+    "http://localhost:8000"
+)
 
 
 st.title(
